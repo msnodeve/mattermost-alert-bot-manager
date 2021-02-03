93c6787e762a28a8a5cb24b69f8aaae8ba08c167
msnodeve <msnodeve@gmail.com>
2021-02-03 13:57:12 +0900

    test: complete scheduler test

--- src/main/java/kr/co/seok/Application.java
+++ src/main/java/kr/co/seok/Application.java
@@ -5,11 +5,17 @@ import kr.co.seok.utils.Holidays;
 import kr.co.seok.utils.LunarCalendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
@@ -21,4 +27,18 @@ public class Application {
             System.out.println(arr.get(i));
         }
     }
+
+    // 단일 쓰레드로 우선 코드 작성
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
+
+//    @Bean
+//    public TaskScheduler taskScheduler() {
+//        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+//        taskScheduler.setPoolSize(10);
+//
+//        return taskScheduler;
+//    }
 }
