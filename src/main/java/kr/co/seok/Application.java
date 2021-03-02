a7c8b9351badf15defaf62cb98b461f934ddcfca
Myeongseok Kim <msnodeve@gmail.com>
2021-03-02 15:05:47 +0900

    release: v1.0
    version 1.0 배포 완료
    사용법 제작 완료

--- src/main/java/kr/co/seok/Application.java
+++ src/main/java/kr/co/seok/Application.java
@@ -1,16 +1,11 @@
 package kr.co.seok;
 
-import com.ibm.icu.util.Calendar;
-import kr.co.seok.utils.Holidays;
-import kr.co.seok.utils.LunarCalendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableScheduling
--- src/main/java/kr/co/seok/controller/FileController.java
+++ src/main/java/kr/co/seok/controller/FileController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin("*")
 @Api(value = "Image-Controller", description = "This is image controller capable of CRUD")
-@RequestMapping(value = "/api/v1/file")
+@RequestMapping(value = "/v1/file")
 @RestController
 public class FileController {
     @Autowired
